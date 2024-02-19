ba4a241647fb6183711c6ecc64340026b184cd23
Filip Pacanowski <fpacanowski@gmail.com>
2024-02-19 16:33:40 +0100

    cleanup

--- ecommerce/payments/lib/payments.rb
+++ ecommerce/payments/lib/payments.rb
@@ -1,11 +1,4 @@
 require "infra"
-require_relative "payments/commands"
-require_relative "payments/events"
-require_relative "payments/on_set_payment_amount"
-require_relative "payments/on_authorize_payment"
-require_relative "payments/on_capture_payment"
-require_relative "payments/on_release_payment"
-require_relative "payments/fake_gateway"
 require_relative "payments/payment"
 
 module Payments
@@ -14,12 +7,14 @@ module Payments
     def initialize(repository)
       @repository = repository
     end
+
     def create_payment(order_id, amount)
       @repository.with_aggregate(Payment.new, "Payments$#{order_id}") do |payment|
         payment.create(order_id, amount)
       end
       update_read_model(order_id)
     end
+
     def mark_paid(order_id)
       @repository.with_aggregate(Payment.new, "Payments$#{order_id}") do |payment|
         payment.mark_paid
@@ -32,22 +27,6 @@ module Payments
       ArPayment
         .find_or_initialize_by(order_id: order_id)
         .update!(state: payment.state)
-    end
-  end
-
-  class Configuration
-    def initialize(gateway)
-      @gateway = gateway
-    end
-
-    def call(event_store, command_bus)
-      command_bus.register(
-        AuthorizePayment,
-        OnAuthorizePayment.new(event_store, @gateway)
-      )
-      command_bus.register(CapturePayment, OnCapturePayment.new(event_store))
-      command_bus.register(ReleasePayment, OnReleasePayment.new(event_store))
-      command_bus.register(SetPaymentAmount, OnSetPaymentAmount.new(event_store))
     end
   end
 end
--- ecommerce/payments/lib/payments/payment.rb
+++ ecommerce/payments/lib/payments/payment.rb
@@ -2,45 +2,25 @@ module Payments
   class Payment
     include AggregateRoot
 
+    class PaymentCreated < Infra::Event; end
+    class PaymentPaid < Infra::Event; end  
+
     AlreadyAuthorized = Class.new(StandardError)
     NotAuthorized = Class.new(StandardError)
     AlreadyCaptured = Class.new(StandardError)
     AlreadyReleased = Class.new(StandardError)
-    Foo = Class.new(StandardError)
+    NonExisting = Class.new(StandardError)
 
     attr_reader :order_id
     attr_reader :state
-
-    def set_amount(order_id, amount)
-      apply(PaymentAmountSet.new(data: { order_id: order_id, amount: amount }))
-    end
 
     def create(order_id, amount)
       apply(PaymentCreated.new(data: { order_id: order_id, amount: amount }))
     end
 
     def mark_paid
-      raise Foo unless order_id
+      raise NonExisting unless order_id
       apply(PaymentPaid.new(data: { order_id: order_id }))
-    end
-
-    def authorize(order_id, gateway)
-      raise AlreadyAuthorized if authorized?
-      gateway.authorize_transaction(order_id, @amount)
-      apply(PaymentAuthorized.new(data: { order_id: order_id }))
-    end
-
-    def capture
-      raise AlreadyCaptured if captured?
-      raise NotAuthorized unless authorized?
-      apply(PaymentCaptured.new(data: { order_id: @order_id }))
-    end
-
-    def release
-      raise AlreadyReleased if released?
-      raise AlreadyCaptured if captured?
-      raise NotAuthorized unless authorized?
-      apply(PaymentReleased.new(data: { order_id: @order_id }))
     end
 
     private
@@ -53,35 +33,6 @@ module Payments
 
     on PaymentPaid do |event|
       @state = :paid
-    end
-
-    on PaymentAmountSet do |event|
-      @amount = event.data.fetch(:amount)
-    end
-
-    on PaymentAuthorized do |event|
-      @state = :authorized
-      @order_id = event.data.fetch(:order_id)
-    end
-
-    on PaymentCaptured do |event|
-      @state = :captured
-    end
-
-    on PaymentReleased do |event|
-      @state = :released
-    end
-
-    def authorized?
-      @state.equal?(:authorized)
-    end
-
-    def captured?
-      @state.equal?(:captured)
-    end
-
-    def released?
-      @state.equal?(:released)
     end
   end
 end
