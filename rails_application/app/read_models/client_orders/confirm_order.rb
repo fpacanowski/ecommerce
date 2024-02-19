845576893c04d97a59df08cfecd1f66289d91625
Filip Pacanowski <fpacanowski@gmail.com>
2024-02-19 17:23:02 +0100

    drop read models

--- rails_application/app/read_models/client_orders/confirm_order.rb
+++
@@ -1,9 +0,0 @@
-module ClientOrders
-  class ConfirmOrder < Infra::EventHandler
-    def call(event)
-      order = Order.find_by(order_uid: event.data.fetch(:order_id))
-      order.state = "Paid"
-      order.save!
-    end
-  end
-end
